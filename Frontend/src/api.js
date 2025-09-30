b6385fa556ea1b9e0d2d6896be7c1d93c5a64018
Vo-Hoa-Thuan <vohothuan2828@gmail.com>
2025-09-30 16:04:57 +0700

    fix run vercel

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,8 +1,8 @@
 
-// const api_auth='https://classhub-efwd.onrender.com/classhub/auth';
-// const api='https://classhub-efwd.onrender.com/classhub';
+const api_auth='https://classhub-efwd.onrender.com/classhub/auth';
+const api='https://classhub-efwd.onrender.com/classhub';
 
-const api_auth='http://localhost:8080/classhub/auth';
-const api='http://localhost:8080/classhub';
+// const api_auth='http://localhost:8080/classhub/auth';
+// const api='http://localhost:8080/classhub';
 
 export {api_auth,api};
